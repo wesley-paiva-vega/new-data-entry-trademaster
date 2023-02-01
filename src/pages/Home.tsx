17e3fa3e1d468c815c506265ce1943d80d5885a3
mhs_wpaiva <mhs_wpaiva@uolinc.com>
2023-02-01 16:05:19 -0300

    add title of page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,25 @@
+import { styled } from '$/theme';
+
+const Main = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  gap: 250,
+  backgroundColor: 'LightCyan',
+
+  h1: {
+    padding: 30,
+  },
+});
 const Home = () => {
-  return <div></div>;
+  return (
+    <Main>
+      <header>
+        <h1>Teste de Performance com React Query</h1>
+      </header>
+
+      <div>Cards Of Marvel App</div>
+    </Main>
+  );
 };
 
 export default Home;
