a5120097db154802f53af503a9238de6a7c10800
mhs_wpaiva <mhs_wpaiva@uolinc.com>
2023-02-01 17:53:33 -0300

    add title of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useEffect } from 'react';
+import { useQuery } from 'react-query';
+
 import Home from '$/pages/Home';
 
+import { getDataMarvel } from './services/Marvel/getDataMarvel';
 import { globalCss } from './theme';
+import { CHARACTER } from './utils/constants';
 
 const Main = globalCss({
   '*': { margin: 0, padding: 0, boxSizing: 'border-box' },
 });
 
 function App() {
+  const { data } = useQuery('characters', () => getDataMarvel.get(CHARACTER));
+
+  useEffect(() => {
+    console.log(data, 'dados pegos no react query');
+  }, [data]);
+
   return (
     <div className={Main()}>
       <Home />
